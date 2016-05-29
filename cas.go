1a804d6fe82ddecf84c3e1664bf595bbad222d75
cmwslw <cwalker32@gmail.com>
2016-05-29 15:23:45 -0400

    stuff.

--- cas.go
+++ cas.go
@@ -13,16 +13,24 @@ type Float struct {
 }
 
 func (f Float) Eval() Ex {
-	f.val = 6
 	return f
 }
 
+type Add struct {
+	addends []Ex
+}
+
+func (a Add) Eval() Ex {
+	return a
+}
+
 func main() {
-	fmt.Println("Hi")
-	//var x Ex
-	//x := Float{5.5}
 	var f Float = Float{5.5}
 	fmt.Println(f)
 	f = f.Eval().(Float)
 	fmt.Println(f)
+	var a = Add{[]Ex{Float{1}, Float{2}}}
+	fmt.Println(a)
+	ae := a.Eval()
+	fmt.Println(ae)
 }
