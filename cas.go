7d5a78fc3d22dfc1b73e1f9856c9f986699f6517
cmwslw <cwalker32@gmail.com>
2016-05-29 15:02:42 -0400

    Stuff.

--- cas.go
+++ cas.go
@@ -1,35 +1,28 @@
 package main
 
 import (
-        "container/list"
-        "fmt"
-        "reflect"
+	"fmt"
 )
 
+type Ex interface {
+	Eval() Ex
+}
+
+type Float struct {
+	val float64
+}
+
+func (f Float) Eval() Ex {
+	f.val = 6
+	return f
+}
+
 func main() {
-        // Create a new list and put some numbers in it.
-        l := list.New()
-        e4 := l.PushBack(4)
-        e1 := l.PushFront(1)
-        l.InsertBefore(3, e4)
-        l.InsertAfter("Hello", e1)
-
-        // Iterate through list and print its contents.
-        for e := l.Front(); e != nil; e = e.Next() {
-                fmt.Println(e.Value)
-                fmt.Println(reflect.TypeOf(e.Value))
-                fmt.Println(reflect.TypeOf(e.Value)==int)
-                fmt.Println(reflect.TypeOf(e.Value))
-        }
-        switch v := anything.(type) {
-                case string:
-                        fmt.Println(v)
-                case int32, int64:
-                        fmt.Println(v)
-                case SomeCustomType:
-                        fmt.Println(v)
-                default:
-                        fmt.Println("unknown")
-        }
-
+	fmt.Println("Hi")
+	//var x Ex
+	//x := Float{5.5}
+	var f Float = Float{5.5}
+	fmt.Println(f)
+	f = f.Eval().(Float)
+	fmt.Println(f)
 }
