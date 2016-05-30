63e0351540583e30d76f166fbd5f226e25bfd744
cmwslw <cwalker32@gmail.com>
2016-05-29 23:34:24 -0400

    Checkpoint.

--- cas.go
+++ cas.go
@@ -1,20 +1,22 @@
 package cas
 
 //import "fmt"
+//import "reflect"
 
 type Ex interface {
-	Eval() Ex
+	Eval()
 }
 
 type Float struct {
-	val float64
+	Val float64
 }
 
-func (f Float) Eval() Ex {
-	f.val = 99
-	return f
+func (f *Float) Eval() {
+	//f.Val += 1
+	//return f
 }
 
+/*
 type Add struct {
 	addends []Ex
 }
@@ -23,5 +25,25 @@ func (a Add) Eval() Ex {
 	for i := range a.addends {
 		a.addends[i] = a.addends[i].Eval()
 	}
+
+	var lastf *Float = nil
+	for _, e := range a.addends {
+		fmt.Println(reflect.TypeOf(e))
+		fmt.Println(reflect.TypeOf(a.addends))
+		f := e.(*Float)
+		ok := true
+		if ok {
+			fmt.Println(f.Val)
+			if lastf != nil {
+				f.Val += lastf.Val;
+				//a.addends[i] = f
+				lastf.Val = 0
+			}
+			fmt.Println(f.Val)
+			lastf = f
+		}
+	}
+
 	return a
 }
+*/
--- cas_test.go
+++ cas_test.go
@@ -4,18 +4,20 @@ import "testing"
 import "fmt"
 
 func Test(t *testing.T) {
-	var f Float = Float{5.5}
+	var f *Float = &Float{5.5}
 	fmt.Println(f)
-	f = f.Eval().(Float)
+	f.Eval()
 	fmt.Println(f)
+	/*
 	var a = Add{[]Ex{
-		Add{[]Ex{
-			Float{80},
-			Float{3},
+		&Add{[]Ex{
+			&Float{80},
+			&Float{3},
 		}},
-		Float{2},
+		&Float{2},
 	}}
 	fmt.Println(a)
 	ae := a.Eval()
 	fmt.Println(ae)
+	*/
 }
