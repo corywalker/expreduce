11471b723cce59be6ad9ca0b4032decf218dfa83
Cory Walker <cwalker32@gmail.com>
2016-06-15 10:22:29 -0400

    Code and tests for exponents.

--- cas.go
+++ cas.go
@@ -273,3 +273,49 @@ func (this *Variable) IsEqual(other Ex) string {
 	}
 	return "EQUAL_TRUE"
 }
+
+// An exponent expression with a base and an exponent
+type Exponent struct {
+	base Ex
+	exponent Ex
+}
+
+func (this *Exponent) Eval() Ex {
+	// Start by evaluating each part
+	this.base = this.base.Eval()
+	this.exponent = this.exponent.Eval()
+
+	// TODO: Handle cases like float raised to the float and things raised to
+	// zero and 1
+
+	return this
+}
+
+func (this *Exponent) ToString() string {
+	var buffer bytes.Buffer
+	buffer.WriteString(this.base.ToString())
+	buffer.WriteString("^")
+	buffer.WriteString(this.exponent.ToString())
+	return buffer.String()
+}
+
+func (this *Exponent) IsEqual(otherEx Ex) string {
+	thisEx := this.Eval()
+	otherEx = otherEx.Eval()
+	this, ok := thisEx.(*Exponent)
+	if !ok {
+		return thisEx.IsEqual(otherEx)
+	}
+	other, ok := otherEx.(*Exponent)
+	if !ok {
+		return "EQUAL_FALSE"
+	}
+	// TODO: Could be improved by knowing about base conversions and logarithms
+	var baseEqual = this.base.IsEqual(other.base) == "EQUAL_TRUE"
+	var exponentEqual = this.exponent.IsEqual(other.exponent) == "EQUAL_TRUE"
+
+	if baseEqual && exponentEqual {
+		return "EQUAL_TRUE"
+	}
+	return "EQUAL_UNK"
+}
--- cas_test.go
+++ cas_test.go
@@ -169,4 +169,22 @@ func Test(t *testing.T) {
 	fmt.Println(withVar.ToString())
 	withVar.Eval()
 	fmt.Println(withVar.ToString())
+
+	var t9 = &Exponent{
+		&Variable{"x"},
+		&Float{2},
+	}
+	var t10 = &Exponent{
+		&Variable{"x"},
+		&Add{[]Ex{&Float{-1}, &Float{3}}},
+	}
+	var t11 = &Exponent{
+		&Variable{"x"},
+		&Float{3},
+	}
+	assert.Equal(t, t9.ToString(), "x^2")
+	assert.Equal(t, t10.ToString(), "x^(-1 + 3)")
+	assert.Equal(t, t11.ToString(), "x^3")
+	assert.Equal(t, "EQUAL_TRUE", t9.IsEqual(t10))
+	assert.Equal(t, "EQUAL_UNK", t9.IsEqual(t11))
 }
