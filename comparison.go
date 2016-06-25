7ce85362bb3c59b9c7d63ecee25c9e356b04476d
cmwslw <cwalker32@gmail.com>
2016-06-25 13:58:03 -0500

    Added quick attempt at SameQ. Obviously doesn't work. Added tests that currently fail.

--- comparison.go
+++ comparison.go
@@ -10,7 +10,7 @@ type Equal struct {
 func (this *Equal) Eval(es *EvalState) Ex {
 	var isequal string = this.Lhs.Eval(es).IsEqual(this.Rhs.Eval(es), es)
 	if isequal == "EQUAL_UNK" {
-		return &Error{"Encountered EQUAL_UNK when comparing for the Equal."}
+		return this
 	} else if isequal == "EQUAL_TRUE" {
 		return &Symbol{"True"}
 	} else if isequal == "EQUAL_FALSE" {
@@ -36,6 +36,45 @@ func (this *Equal) IsEqual(otherEx Ex, es *EvalState) string {
 
 func (this *Equal) DeepCopy() Ex {
 	return &Equal{
+		this.Lhs.DeepCopy(),
+		this.Rhs.DeepCopy(),
+	}
+}
+
+type SameQ struct {
+	Lhs Ex
+	Rhs Ex
+}
+
+func (this *SameQ) Eval(es *EvalState) Ex {
+	var isequal string = this.Lhs.Eval(es).IsEqual(this.Rhs.Eval(es), es)
+	if isequal == "EQUAL_UNK" {
+		return &Symbol{"False"}
+	} else if isequal == "EQUAL_TRUE" {
+		return &Symbol{"True"}
+	} else if isequal == "EQUAL_FALSE" {
+		return &Symbol{"False"}
+	}
+
+	return &Error{"Unexpected equality return value."}
+}
+
+func (this *SameQ) ToString() string {
+	var buffer bytes.Buffer
+	buffer.WriteString("(")
+	buffer.WriteString(this.Lhs.ToString())
+	buffer.WriteString(") === (")
+	buffer.WriteString(this.Rhs.ToString())
+	buffer.WriteString(")")
+	return buffer.String()
+}
+
+func (this *SameQ) IsEqual(otherEx Ex, es *EvalState) string {
+	return "EQUAL_UNK"
+}
+
+func (this *SameQ) DeepCopy() Ex {
+	return &SameQ{
 		this.Lhs.DeepCopy(),
 		this.Rhs.DeepCopy(),
 	}
--- comparison_test.go
+++ comparison_test.go
@@ -20,6 +20,22 @@ func TestComparison(t *testing.T) {
 	assert.Equal(t, "False", EvalInterp("tmp==6", es).ToString())
 	assert.Equal(t, "False", EvalInterp("6==tmp", es).ToString())
 
-	//assert.Equal(t, "a==b", EvalInterp("a==b", es).ToString())
-	//assert.Equal(t, "a==5", EvalInterp("a==5", es).ToString())
+	assert.Equal(t, "False", EvalInterp("a===b", es).ToString())
+	assert.Equal(t, "True", EvalInterp("a===a", es).ToString())
+	assert.Equal(t, "a==b", EvalInterp("a==b", es).ToString())
+	assert.Equal(t, "True", EvalInterp("a==a", es).ToString())
+	assert.Equal(t, "a==2", EvalInterp("a==2", es).ToString())
+	assert.Equal(t, "True", EvalInterp("tmp==5", es).ToString())
+	assert.Equal(t, "True", EvalInterp("tmp===5", es).ToString())
+	assert.Equal(t, "True", EvalInterp("tmp==5.", es).ToString())
+	assert.Equal(t, "True", EvalInterp("tmp==5.00000", es).ToString())
+	assert.Equal(t, "False", EvalInterp("tmp===5.", es).ToString())
+	assert.Equal(t, "True", EvalInterp("1+1==2", es).ToString())
+	assert.Equal(t, "True", EvalInterp("1+1===2", es).ToString())
+	assert.Equal(t, "y==m*x+b", EvalInterp("y==m*x+b", es).ToString())
+	assert.Equal(t, "False", EvalInterp("y===m*x+b", es).ToString())
+
+	// Future
+	//assert.Equal(t, "False", EvalInterp("4/3==3/2", es).ToString())
+	//assert.Equal(t, "True", EvalInterp("4/3==8/6", es).ToString())
 }
