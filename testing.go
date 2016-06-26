fa3a5c6fa35818553c2f4028012c112e9e272d2d
cmwslw <cwalker32@gmail.com>
2016-06-26 09:51:32 -0500

    Add CAS-specific testing functions.

---
+++ testing.go
@@ -0,0 +1,40 @@
+package cas
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"bytes"
+)
+
+func CasTestInner(es *EvalState, out string, in string, test bool) (succ bool, s string) {
+	inTree := EvalInterp(in, es)
+	outTree := Interp(out)
+	theTestTree := SameQ{Lhs: inTree, Rhs: outTree}
+	theTest := theTestTree.Eval(es)
+
+	var buffer bytes.Buffer
+	buffer.WriteString("(")
+	buffer.WriteString(inTree.ToString())
+	if test {
+		buffer.WriteString(") != (")
+	} else {
+		buffer.WriteString(") == (")
+	}
+	buffer.WriteString(outTree.ToString())
+	buffer.WriteString(")")
+
+	if test {
+		return (theTest.ToString() == "True"), buffer.String()
+	}
+	return (theTest.ToString() == "False"), buffer.String()
+}
+
+func CasAssertSame(t *testing.T, es *EvalState, out string, in string) {
+	succ, s := CasTestInner(es, out, in, true)
+	assert.True(t, succ, s)
+}
+
+func CasAssertDiff(t *testing.T, es *EvalState, out string, in string) {
+	succ, s := CasTestInner(es, out, in, false)
+	assert.True(t, succ, s)
+}
---
+++ testing_test.go
@@ -0,0 +1,22 @@
+
+package cas
+
+import (
+	"testing"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTesting(t *testing.T) {
+	fmt.Println("Testing testing")
+
+	es := NewEvalState()
+
+	CasAssertSame(t, es, " 1 ", "    1")
+	succ, s := CasTestInner(es, " 1. ", "1  ", true)
+	assert.False(t, succ, s)
+	CasAssertSame(t, es, "5.5", "1+1.5+3")
+	CasAssertDiff(t, es, "5.6", "1+1.5+3")
+	CasAssertSame(t, es, "9", "If[True, 9, 10]")
+	CasAssertDiff(t, es, " 1. ", "    1")
+}
