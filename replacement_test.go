0427e1e02242f0b6321c9b98875f244205f40ebf
cmwslw <cwalker32@gmail.com>
2016-06-29 19:57:21 -0400

    Add some more tests for replacement.

--- replacement_test.go
+++ replacement_test.go
@@ -14,4 +14,12 @@ func TestReplacement(t *testing.T) {
 
 	assert.Equal(t, "((1) == ((2 * 5^-1))) /. (((2) -> (3)) == (x))", Interp("1 == 2/5 /. 2 -> 3 == x").ToString())
 	assert.Equal(t, "2^(y+1)", EvalInterp("2^(x^2+1) /. x^2->y", es).ToString())
+	CasAssertSame(t, es, "b + c + d", "a + b + c + c^2 /. c^2 + a -> d")
+	CasAssertSame(t, es, "a b c", "a*b*c /. c + a -> d")
+	CasAssertSame(t, es, "b d", "a*b*c /. c*a -> d")
+	CasAssertSame(t, es, "2 a + b + c + c^2", "2 a + b + c + c^2 /. c^2 + a -> d")
+	CasAssertSame(t, es, "2 a + b + c + c^2", "a + a + b + c + c^2 /. c^2 + a -> d")
+	CasAssertSame(t, es, "a b c + a b^2 c", "(a*b*c) + (a*b^2*c)")
+	CasAssertSame(t, es, "b d + b^2 d", "(a*b*c) + (a*b^2*c) /. c*a -> d")
+	CasAssertSame(t, es, "a + b + c", "a + b + c /. c + a -> c + a")
 }
