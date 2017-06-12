37ff9b8e207582168070916386cd1c4d848b5ddb
cmwslw <cwalker32@gmail.com>
2017-06-11 17:22:11 -0700

    Iterator function for form allocations.

---
+++ expreduce/allocations.go
@@ -0,0 +1,52 @@
+package expreduce
+
+type allocIterState struct {
+	currForm int
+	remaining int
+	currFormI int
+}
+
+type allocIter struct {
+	forms []parsedForm
+	alloc []int
+	stack []allocIterState
+}
+
+// Returns if should continue, result is stored in ai.alloc.
+func (ai *allocIter) next() bool {
+	for len(ai.stack) > 0 {
+		var p allocIterState
+		l := len(ai.stack)
+		ai.stack, p = ai.stack[:l-1], ai.stack[l-1]
+		if p.currForm-1 >= 0 {
+			ai.alloc[p.currForm-1] = p.currFormI
+		}
+		if p.currForm >= len(ai.forms) {
+			return true
+		}
+		for i := ai.forms[p.currForm].endI; i >= ai.forms[p.currForm].startI; i-- {
+			if p.currForm+1 >= len(ai.forms) {
+				if p.remaining-i == 0 {
+					ai.stack = append(ai.stack, allocIterState{
+					p.currForm+1, p.remaining-i, i})
+				}
+			} else {
+				if p.remaining-i >= 0 {
+					ai.stack = append(ai.stack, allocIterState{
+					p.currForm+1, p.remaining-i, i})
+				}
+			}
+		}
+	}
+	return false
+}
+
+func NewAllocIter(l int, forms []parsedForm) allocIter {
+	ai := allocIter{}
+	ai.forms = forms
+	ai.alloc = make([]int, len(forms))
+	ai.stack = []allocIterState{
+		allocIterState{0, l, 0},
+	}
+	return ai
+}
---
+++ expreduce/allocations_test.go
@@ -0,0 +1,81 @@
+package expreduce
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func newPf(startI int, endI int) parsedForm {
+	return parsedForm{
+		startI, endI,
+		nil, nil,
+		false, false, false,
+	}
+}
+
+func TestAllocations(t *testing.T) {
+	fmt.Println("Testing allocations")
+
+	forms := []parsedForm{
+		newPf(1, 2),
+		newPf(0, 1),
+		newPf(0, 99999),
+	}
+	ai := NewAllocIter(4, forms)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{1, 0, 3}, ai.alloc)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{1, 1, 2}, ai.alloc)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{2, 0, 2}, ai.alloc)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{2, 1, 1}, ai.alloc)
+	assert.Equal(t, false, ai.next())
+
+	forms = []parsedForm{
+		newPf(1, 1),
+		newPf(0, 99999),
+	}
+	ai = NewAllocIter(4, forms)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{1, 3}, ai.alloc)
+	assert.Equal(t, false, ai.next())
+
+	forms = []parsedForm{
+		newPf(0, 99999),
+		newPf(1, 1),
+		newPf(0, 99999),
+	}
+	ai = NewAllocIter(4, forms)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{0, 1, 3}, ai.alloc)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{1, 1, 2}, ai.alloc)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{2, 1, 1}, ai.alloc)
+	assert.Equal(t, true, ai.next())
+	assert.Equal(t, []int{3, 1, 0}, ai.alloc)
+	assert.Equal(t, false, ai.next())
+
+	// Impossible configuration. Should return false immediately.
+	forms = []parsedForm{
+		newPf(5, 5),
+	}
+	ai = NewAllocIter(4, forms)
+	assert.Equal(t, false, ai.next())
+
+	// Impossible configuration. Should return false immediately.
+	forms = []parsedForm{
+		newPf(1, 1),
+	}
+	ai = NewAllocIter(4, forms)
+	assert.Equal(t, false, ai.next())
+
+	// Impossible configuration. Should return false immediately.
+	forms = []parsedForm{
+		newPf(5, 0),
+	}
+	ai = NewAllocIter(4, forms)
+	assert.Equal(t, false, ai.next())
+}
