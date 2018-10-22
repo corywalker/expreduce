5b4caedd037a15967b68dc961acf0daeae049421
cmwslw <cwalker32@gmail.com>
2018-10-22 00:48:03 -0700

    Tests should be fixed.

---
+++ expreduce/interp_test.go
@@ -0,0 +1,74 @@
+package expreduce
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/corywalker/expreduce/expreduce/parser"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInterp(t *testing.T) {
+	fmt.Println("Testing interp")
+
+	es := NewEvalState()
+
+	casAssertSame(t, es, "2*x", "2x")
+	casAssertSame(t, es, "2*x+5*y", "2x+5y")
+	casAssertSame(t, es, "2*x+5*y", "2 x+5 y")
+	casAssertSame(t, es, "2*x+5*foo[x]", "2x+5foo[x]")
+	casAssertSame(t, es, "2*x+5*foo[x]", "2x+5 foo[x]")
+
+	casAssertSame(t, es, "{x, x, g[x], g[x]}", "{f[f[x]], f[x], g[f[x]], f[g[f[x]]]} //. f[xmatch_] -> xmatch")
+	casAssertSame(t, es, "foo[{x, x, g[x], g[x]}]", "{f[f[x]], f[x], g[f[x]], f[g[f[x]]]} //. f[xmatch_] -> xmatch // foo")
+	casAssertSame(t, es, "3[P[1[2]]]", "P@1@2//3")
+	// TODO: Currently does not work:
+	//casAssertSame(t, es, "(x^2)*y", "x^2 y")
+
+	// Test Slots
+	casAssertSame(t, es, "Slot[1]", "#")
+	casAssertSame(t, es, "Slot[2]", "#2")
+	casAssertSame(t, es, "3*Slot[2]", "3#2")
+
+	// Test PatternTest
+	casAssertSame(t, es, "PatternTest[a,b]", "a?b")
+	//casAssertSame(t, es, "PatternTest[foo[a], bar][b]", "foo[a]?bar[b]")
+	casAssertSame(t, es, "PatternTest[foo[a], bar[b]]", "foo[a]?(bar[b])")
+	casAssertSame(t, es, "PatternTest[Pattern[a, Blank[Integer]], NumberQ]", "a_Integer?NumberQ")
+	casAssertSame(t, es, "PatternTest[Pattern[a, Blank[Integer]], Function[Divisible[Slot[1], 7]]]", "a_Integer?(Function[Divisible[#, 7]])")
+
+	// Test precedence of equality, rules, and ReplaceAll
+	casAssertSame(t, es, "Hold[ReplaceAll[Equal[1, 2], Rule[2, Equal[3, x]]]]", "Hold[1 == 2 /. 2 -> 3 == x]")
+
+	// Test Condition
+	casAssertSame(t, es, "Condition[a,b]", "a/;b")
+	casAssertSame(t, es, "Hold[Condition[a,b]]", "Hold[a/;b]")
+	//casAssertSame(t, es, "Hold[CompoundExpression[Condition[a,b],Condition[a,b]]]", "Hold[a/;b ; a/;b]")
+	casAssertSame(t, es, "Hold[Condition[List[Pattern[x, Blank[]], Pattern[x, Blank[]]], Equal[Plus[x, x], 2]]]", "Hold[{x_,x_}/;x+x==2]")
+	casAssertSame(t, es, "Hold[SetDelayed[foo[Pattern[x, Blank[]]], Condition[bar[x], Equal[x, 0]]]]", "Hold[foo[x_] := bar[x] /; x == 0]")
+	casAssertSame(t, es, "Hold[ReplaceAll[List[5, 0, -5], Rule[Condition[Pattern[y, Blank[]], Equal[y, 0]], z]]]", "Hold[{5, 0, -5} /. y_ /; y == 0 -> z]")
+
+	// Test MessageName
+	casAssertSame(t, es, "Hold[MessageName[a,\"b\"]]", "Hold[a::b]")
+	casAssertSame(t, es, "MessageName[a,\"b\"]", "a::b")
+
+	// Test StringJoin
+	casAssertSame(t, es, "StringJoin[\"a\", \" world\", \"hi\"]", "\"a\" <> \" world\" <> \"hi\"")
+
+	// Test Not and Factorial
+	casAssertSame(t, es, "Factorial[a]", "a!")
+	casAssertSame(t, es, "Not[a]", "!a")
+	casAssertSame(t, es, "Factorial[a]*b", "a!b")
+
+	// Test Optional and Pattern
+	// Currently disabled because issue #79
+	//casAssertSame(t, es, "Plus[a,Pattern[a,5]]", "a + a : 5")
+	//casAssertSame(t, es, "Plus[a,Optional[Pattern[a,Blank[]],5]]", "a + a_ : 5")
+	//casAssertSame(t, es, "Plus[Times[2,a],Optional[Pattern[a,Blank[]],5]]", "a + a_ : 5 + a")
+
+	// Test newline handling
+	assert.Equal(t, "CompoundExpression[a, b]", parser.Interp("a;b\n", es).String(es))
+	//assert.Equal(t, "Sequence[a, b]", parser.Interp("a\nb\n", es).String(es))
+	assert.Equal(t, "(c = a*b)", parser.Interp("c = (a\nb)\n", es).String(es))
+	assert.Equal(t, "(c = a*b)", parser.Interp("c = (a\n\nb)\n", es).String(es))
+}
--- expreduce/matcher/allocations_test.go
+++ expreduce/matcher/allocations_test.go
@@ -33,7 +33,7 @@ func TestAllocations(t *testing.T) {
 		newPf(0, 1),
 		newPf(0, 99999),
 	}
-	ai := NewAllocIter(4, forms)
+	ai := newAllocIter(4, forms)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, []int{1, 0, 3}, ai.alloc)
 	assert.Equal(t, true, ai.next())
@@ -48,7 +48,7 @@ func TestAllocations(t *testing.T) {
 		newPf(1, 1),
 		newPf(0, 99999),
 	}
-	ai = NewAllocIter(4, forms)
+	ai = newAllocIter(4, forms)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, []int{1, 3}, ai.alloc)
 	assert.Equal(t, false, ai.next())
@@ -58,7 +58,7 @@ func TestAllocations(t *testing.T) {
 		newPf(1, 1),
 		newPf(0, 99999),
 	}
-	ai = NewAllocIter(4, forms)
+	ai = newAllocIter(4, forms)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, []int{1, 1, 2}, ai.alloc)
 	assert.Equal(t, false, ai.next())
@@ -70,7 +70,7 @@ func TestAllocations(t *testing.T) {
 	}
 	forms[0].isOptional = true
 	forms[1].isOptional = true
-	ai = NewAllocIter(3, forms)
+	ai = newAllocIter(3, forms)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, []int{1, 1, 1}, ai.alloc)
 	assert.Equal(t, true, ai.next())
@@ -86,7 +86,7 @@ func TestAllocations(t *testing.T) {
 		newPf(1, 1),
 		newPf(0, 99999),
 	}
-	ai = NewAllocIter(4, forms)
+	ai = newAllocIter(4, forms)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, []int{0, 1, 3}, ai.alloc)
 	assert.Equal(t, true, ai.next())
@@ -100,7 +100,7 @@ func TestAllocations(t *testing.T) {
 	forms = []parsedForm{
 		newPf(0, 99999),
 	}
-	ai = NewAllocIter(0, forms)
+	ai = newAllocIter(0, forms)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, []int{0}, ai.alloc)
 	assert.Equal(t, false, ai.next())
@@ -108,28 +108,28 @@ func TestAllocations(t *testing.T) {
 	forms = []parsedForm{
 		newPf(1, 99999),
 	}
-	ai = NewAllocIter(0, forms)
+	ai = newAllocIter(0, forms)
 	assert.Equal(t, false, ai.next())
 
 	// Impossible configuration. Should return false immediately.
 	forms = []parsedForm{
 		newPf(5, 5),
 	}
-	ai = NewAllocIter(4, forms)
+	ai = newAllocIter(4, forms)
 	assert.Equal(t, false, ai.next())
 
 	// Impossible configuration. Should return false immediately.
 	forms = []parsedForm{
 		newPf(1, 1),
 	}
-	ai = NewAllocIter(4, forms)
+	ai = newAllocIter(4, forms)
 	assert.Equal(t, false, ai.next())
 
 	// Impossible configuration. Should return false immediately.
 	forms = []parsedForm{
 		newPf(5, 0),
 	}
-	ai = NewAllocIter(4, forms)
+	ai = newAllocIter(4, forms)
 	assert.Equal(t, false, ai.next())
 
 	forms = []parsedForm{
@@ -137,7 +137,7 @@ func TestAllocations(t *testing.T) {
 		newPf(1, 1),
 		newPf(0, 999999),
 	}
-	ai = NewAllocIter(800000, forms)
+	ai = newAllocIter(800000, forms)
 	num := 0
 	for num = 0; ai.next(); num++ {
 	}
@@ -151,7 +151,7 @@ func TestAllocations(t *testing.T) {
 		newPf(1, 1),
 		newPf(0, 999999),
 	}
-	ai = NewAllocIter(1400, forms)
+	ai = newAllocIter(1400, forms)
 	for num = 0; ai.next(); num++ {
 	}
 	assert.Equal(t, 979300, num)
@@ -177,7 +177,7 @@ func TestAssignments(t *testing.T) {
 		newPf(0, 99999),
 	}
 	nComps := 3
-	ai := NewAssnIter(nComps, forms, allMatch(nComps, len(forms)), true)
+	ai := newAssnIter(nComps, forms, allMatch(nComps, len(forms)), true)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, [][]int{{}, {0}, {1, 2}}, ai.assns)
 	assert.Equal(t, true, ai.next())
@@ -198,7 +198,7 @@ func TestAssignments(t *testing.T) {
 	assert.Equal(t, [][]int{{2}, {1}, {0}}, ai.assns)
 	assert.Equal(t, false, ai.next())
 
-	ai = NewAssnIter(nComps, forms, allMatch(nComps, len(forms)), false)
+	ai = newAssnIter(nComps, forms, allMatch(nComps, len(forms)), false)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, [][]int{{}, {0}, {1, 2}}, ai.assns)
 	assert.Equal(t, true, ai.next())
@@ -217,7 +217,7 @@ func TestAssignments(t *testing.T) {
 		{false, true, false},
 		{true, true, true},
 	}
-	ai = NewAssnIter(nComps, forms, formMatches, true)
+	ai = newAssnIter(nComps, forms, formMatches, true)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, [][]int{{0}, {1}, {2}}, ai.assns)
 	assert.Equal(t, true, ai.next())
@@ -233,7 +233,7 @@ func TestAssignments(t *testing.T) {
 		{true, true, true},
 		{true, false, false},
 	}
-	ai = NewAssnIter(nComps, forms, formMatches, true)
+	ai = newAssnIter(nComps, forms, formMatches, true)
 	assert.Equal(t, true, ai.next())
 	assert.Equal(t, [][]int{{1, 2}, {0}}, ai.assns)
 	assert.Equal(t, false, ai.next())
@@ -247,14 +247,14 @@ func TestAssignments(t *testing.T) {
 		newPf(0, 999999),
 	}
 	nComps = 1400
-	ai = NewAssnIter(nComps, forms, allMatch(nComps, len(forms)), false)
+	ai = newAssnIter(nComps, forms, allMatch(nComps, len(forms)), false)
 	num := 0
 	for num = 0; ai.next(); num++ {
 	}
 	assert.Equal(t, 979300, num)
 
 	nComps = 8
-	ai = NewAssnIter(nComps, forms, allMatch(nComps, len(forms)), true)
+	ai = newAssnIter(nComps, forms, allMatch(nComps, len(forms)), true)
 	for num = 0; ai.next(); num++ {
 	}
 	assert.Equal(t, 40824, num)
