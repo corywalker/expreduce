9516f0d21d2e98aff1c622478af374852349ee55
Cory Walker <cwalker32@gmail.com>
2024-11-30 23:48:53 +0000

    Script to start up a Jupyter Notebook supporting Expreduce.

---
+++ start_jupyter.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+echo "Building Expreduce kernel..."
+go build -o expreduce_for_jupyter
+
+# Set the virtual environment directory
+VENV_DIR="venv"
+
+# Check if the virtual environment exists
+if [ ! -d "$VENV_DIR" ]; then
+  echo "Creating virtual environment..."
+  python3 -m venv "$VENV_DIR"
+fi
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Upgrade pip
+echo "Upgrading pip..."
+pip install --upgrade pip
+
+# Install Jupyter if not already installed
+if ! pip show jupyter > /dev/null 2>&1; then
+  echo "Installing Jupyter..."
+  pip install https://github.com/jupyter/notebook/releases/download/v6.5.7/notebook-6.5.7-py3-none-any.whl
+fi
+
+pip install setuptools
+
+pip install metakernel --upgrade
+#pip install jupyter_contrib_nbextensions
+
+[ -d "iwolfram" ] || git clone https://github.com/mmatera/iwolfram.git
+cd iwolfram
+python setup.py install --mma-exec ../expreduce_for_jupyter
+
+# Run Jupyter Notebook
+echo "Starting Jupyter Notebook..."
+jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser
+
+# Deactivate the virtual environment on exit
+deactivate
